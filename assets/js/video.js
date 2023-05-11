b4c9194eecf287247f5117db140d9332db3eede5
nusamockup <121804038+nusamockup@users.noreply.github.com>
2023-05-12 00:24:26 +0700

    Update video.js

--- assets/js/video.js
+++ assets/js/video.js
@@ -2,7 +2,7 @@
 
 (function () {
   let video =
-      "<iframe class='embed-responsive-item' src='assets/video/sopa-video-2.mp4' autoplay frameborder='0' allowfullscreen></iframe>";
+      "<iframe class='embed-responsive-item' src='https://user-images.githubusercontent.com/121804038/237766347-5e3a1790-3149-45e6-acf5-d29ecb88bc43.mp4' autoplay frameborder='0' allowfullscreen></iframe>";
   let video_frame = $('.video__video');
   video_frame.on('click', function (e) {
     e.preventDefault();
